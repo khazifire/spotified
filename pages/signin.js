af85641aad647c320548ef74451046bfc07fe643
dan <itskazimz@gmail.com>
2022-07-28 17:17:10 +0700

    fix: color on sign in btn fixed

--- pages/signin.js
+++ pages/signin.js
@@ -13,11 +13,11 @@ export default function SignIn({ providers }) {
             <div className='flex flex-col md:flex-row items-center gap-2 md:gap-8'>
                 <div className='flex flex-row items-center'>
                   <i className="ri-blaze-fill text-7xl "></i>
-                  <h1 className='font-semibold text-5xl hidden'>Spotified</h1>
+                  <h1 className='font-semibold text-5xl '>Spotified</h1>
                 </div>
                 {Object.values(providers).map((provider) => (
                 <div key={provider.name} className="">
-                    <button onClick={() => signIn(provider.id)} className="hover:bg-green2  text-black bg-black p-2 rounded-full px-6 font-medium text-sm">LOG IN WITH SPOTIFY</button>
+                    <button onClick={() => signIn(provider.id)} className="hover:bg-green2  text-black bg-green p-2 rounded-full px-6 font-medium text-sm">LOG IN WITH SPOTIFY</button>
                 </div>
                 ))}
             </div>
