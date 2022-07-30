1ce8c73f7f8f9f3238b85b3c41ea2b22c5421c97
Dan <73513866+khazifire@users.noreply.github.com>
2022-07-30 23:14:24 +0700

    removed red-user-email from scope

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ export default NextAuth({
   providers: [
     SpotifyProvider({
       authorization:
-        'https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-read-recently-played,user-top-read,user-follow-read,user-read-currently-playing,user-library-read',
+        'https://accounts.spotify.com/authorize?scope=playlist-read-private,user-read-recently-played,user-top-read,user-follow-read,user-read-currently-playing,user-library-read',
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
     }),
