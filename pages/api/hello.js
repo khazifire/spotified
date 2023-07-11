9c58ed22f68138813dd3d914b15c87e0408e1db2
Dan <73513866+khazifire@users.noreply.github.com>
2023-07-12 01:17:52 +0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,24 +1,8 @@
-import {NextResponse} from "next/server";
+// import {NextResponse} from "next/server";
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// export default function handler(req, res) {
-//   res.status(200).json({ name: 'John Doe' })
-// }
+export default function handler(req, res) {
+  res.status(200).json({ name: 'John Doe' })
+}
 
-export const GET = async (req,res)=>{
-  try{
-    return NextResponse.json({msg: "this is a get method"}, {status:200})
-  }catch(err){
-    return NextResponse.json({msg: err}, {status:500})
-  }
-  
-};
 
-export const POST = async (req,res)=>{
-  const {title, name} = await reg.json()
-  try{
-    return NextResponse.json({name: name, title: title}, {status:200})
-  }catch(err){
-    return NextResponse.json({msg: err}, {status:500})
-  }
-};
