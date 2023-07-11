964b6404f3de1374ecd6fda0fdb68f4a6bca3ae8
Dan <73513866+khazifire@users.noreply.github.com>
2023-07-12 00:58:11 +0700

    testing api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+export const GET = async (req,res){
+  res.status(200).json({msg: "this is a get method"})
+}
+
+export const POST = async (req,res){
+  res.status(200).json({msg: "this is a post method", reg:reg})
 }
